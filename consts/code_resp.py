0006d98fad21a07e1a8dfd5c3f1a8335240f920b
“zhuchen” <zhuchen@hortorgames.com>
2023-07-28 14:20:38 +0800

    feat:部署测试

---
+++ consts/code_resp.py
@@ -0,0 +1,3 @@
+from vo.customer_err import CustomError
+
+Err_System = CustomError(10000, "系统异常")
---
+++ consts/public_consts.py
@@ -0,0 +1,8 @@
+# 客服机器人基础配置刷新
+ComerAiRobotConfigKey = "customer/robot/ai/config"
+# [旧]客服机器人知识库单个问题刷新
+RefreshRobotKnowledgeSingleKey = "customer/robot/ai/knowledge/single"
+# [新]刷新内存知识库信息
+RefreshCacheKnowledgeKey = "customer/robot/ai/cache/knowledge/embedder"
+# [新]定时任务刷新无效问题
+RefreshCacheCrontabKey = "customer/robot/ai/cache/crontab/update"
