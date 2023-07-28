76bc9ccb1cbbcf6060efc318bd565b28466c97b1
“zhuchen” <zhuchen@hortorgames.com>
2023-07-28 14:33:58 +0800

    feat:多显卡支持

--- router/test_router.py
+++ router/test_router.py
@@ -10,7 +10,7 @@ test_routers = APIRouter()
 # 获取答案
 @test_routers.get('/GetAnswer/{key}')
 async def GetAnswer(key: int):
-    msg = "想要增强大语言模型的上下文窗口，可以使用哪些技术手段？"
-    if key == 1:
-        msg = prompt + "\n 根据这篇文章内容，请回答我的问题：想要增强大语言模型的上下文窗口，可以使用哪些技术手段？"
+    # msg = "想要增强大语言模型的上下文窗口，可以使用哪些技术手段？"
+    # if key == 1:
+    msg = prompt + "\n 根据这篇文章内容，请回答我的问题：想要增强大语言模型的上下文窗口，可以使用哪些技术手段？"
     return Response.success(TestService().display_answer(msg))
