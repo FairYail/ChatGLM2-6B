239d62cbb3a3b7f5f92834282bce59fd40d4e7fd
“zhuchen” <zhuchen@hortorgames.com>
2023-07-31 20:19:56 +0800

    feat:prompt 优化

--- service/test_service.py
+++ service/test_service.py
@@ -53,7 +53,9 @@ class TestService:
     @classmethod
     def init_model(cls):
         # 加载大预言模型模型
-        model_path = "/data/chatglm2-6b"
+        # model_path = "/data/chatglm2-6b"
+        model_path = "THUDM/chatglm2-6b-32k"
+        # model_path = "THUDM/chatglm2-12b"
         tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, revision="v1.0")
         # model = load_model_on_gpus(model_path, num_gpus=1)
         model = AutoModel.from_pretrained(model_path, trust_remote_code=True, device='cuda', revision="v1.0")
