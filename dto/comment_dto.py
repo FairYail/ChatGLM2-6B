4c1b6acc2c98854313e4a35519a713a8507115c5
“zhuchen” <zhuchen@hortorgames.com>
2023-08-07 22:31:40 +0800

    feat:代码逻辑优化

--- dto/comment_dto.py
+++ dto/comment_dto.py
@@ -3,10 +3,11 @@ from pydantic import BaseModel
 from consts.code_resp import Err_Param_Info
 
 
-class CommentDto:
+class CommentDto(BaseModel):
+    prompt: str
 
-    def __init__(self, prompt=str):
-        self.prompt = prompt
+    def __init__(self, prompt: str):
+        super().__init__(prompt=prompt)
 
     # 参数校验
     def Validator(self):
