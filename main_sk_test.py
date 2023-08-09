430166a5f06923835d7cd1fb399c934ec7e11cd4
“zhuchen” <zhuchen@hortorgames.com>
2023-08-09 14:39:00 +0800

    feat:代码逻辑优化

--- main_sk_test.py
+++ main_sk_test.py
@@ -105,5 +105,11 @@ if __name__ == "__main__":
     loaded_model = joblib.load(model_filename)
 
     # 使用加载的模型进行预测
-    predictions = loaded_model.predict(['游戏界面清新，音乐也很搭配，玩起来心情愉悦。'])
-    print("Predictions for new data:", predictions)
+    while True:
+        new_data = input("Enter: ")
+        if new_data.lower() == 'exit':
+            break  # 如果输入 'exit'，退出循环
+        vect = CountVectorizer()
+        new_text_vectorized = vect.transform([new_data])  # 将输入的文本向量化
+        predictions = loaded_model.predict(new_text_vectorized)
+        print("Predictions for new data:", predictions)
